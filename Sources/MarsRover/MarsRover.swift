2b9d81c181f2fb4b04e29374d83e1ab4329c0ce5
Johann Behrens <me@jbehrens.nu>
2025-05-02 16:13:57 +0200

    docs: add short documentation to MarsRover

--- Sources/MarsRover/MarsRover.swift
+++ Sources/MarsRover/MarsRover.swift
@@ -1,10 +1,16 @@
 final class MarsRover {
+    /// Internal state of the MarsRover.
+    /// Non-accessible in the code, but readable in tests.
     private(set) var state: MarsRover.State
 
     init(state: MarsRover.State) {
         self.state = state
     }
 
+    /// MarsRover can execute commands. They are defined in `Command`.
+    /// Example: `execute(.left, .right, .left)`.
+    ///
+    /// - Parameter commands: one or more `Command` items. Executed in order, one by one.
     func execute(commands: [MarsRover.Command]) {
         let actions: [MarsRover.Command: () -> Void] = [
             .left: { self.state.turn(clockwise: false) },
